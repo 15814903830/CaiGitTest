ad027d7efbcfb08c5d7a01dd29e4bde3e4c95c10
caiandroid <1273258724@qq.com>
2020-11-16 12:36:27 +0800

    测试提交

---
+++ app/src/main/java/com/zblibrary/caigittest/MainActivity.java
@@ -0,0 +1,21 @@
+package com.zblibrary.caigittest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+/**
+ * 描述 : github提交测试
+ *
+ * @作者 菜Android
+ * @时间 2020年 11月 16日
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //提交测试
+    }
+}
